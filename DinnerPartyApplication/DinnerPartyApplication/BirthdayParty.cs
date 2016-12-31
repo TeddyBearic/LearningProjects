830e6289ae3990ed9eaeab773a612ebe24ad1d3f
TeddyBearic <agraham41325@gmail.com>
2016-12-30 19:19:59 -0800

    added birthdayparty.cs

---
+++ DinnerPartyApplication/DinnerPartyApplication/BirthdayParty.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerPartyApplication
+{
+    class BirthdayParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public decimal Cost;
+        public string CakeWriting { get; set; }
+        public bool CakeWritingTooLong;
+
+        public BirthdayParty(int numberOfPeople, bool fancyDecorations, string cakeWriting)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            CakeWriting = cakeWriting;
+        }
+
+        private void CalculateCostOfDecorations()
+        {
+
+        }
+
+        private int CakeSize()
+        {
+            if (NumberOfPeople <= 4)
+                return 8;
+            else
+                return 16;
+        }
+
+        private int MaxWritingLength()
+        {
+            if (CakeSize() == 8)
+                return 16;
+            else
+                return 40;
+        }
+
+        private int ActualLength
+        {
+            get
+            {
+                if CakeWriting.Length > MaxWritingLength())
+                    return MaxWritingLength();
+                else
+                    return CakeWriting.Length;
+            }
+        }
+    }
+}
