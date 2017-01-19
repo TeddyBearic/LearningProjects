ecd898442b195ff545647098a4c0ddaedf36da79
TeddyBearic <agraham41325@gmail.com>
2017-01-18 18:34:47 -0800

    Beehive Management App

--- BeehiveManager/BeehiveManager/Form1.cs
+++ BeehiveManager/BeehiveManager/Form1.cs
@@ -15,6 +15,30 @@ namespace BeehiveManager
         public Form1()
         {
             InitializeComponent();
+            workerBeeJob.SelectedIndex = 0;
+
+            Worker[] workers = new Worker[4];
+            workers[0] = new Worker(new string[] { "Nector Collector", "Honey Manufacturing" });
+            workers[1] = new Worker(new string[] { "Egg Care", "Baby Bee Tutoring" });
+            workers[2] = new Worker(new string[] { "Hive Maintenance", "Sting Patrol" });
+            workers[3] = new Worker(new string[] { "Nector Collector", "Honey Manufacturing", "Hive Maintenance", "Sting Patrol", "Egg Care", "Baby Bee Tutoring" });
+
+            queen = new Queen(workers);
+        }
+
+            private Queen queen;
+
+        private void assignJob_Click(object sender, EventArgs e)
+        {
+            if (queen.AssignWork(workerBeeJob.Text, (int)shifts.Value) == false)
+                MessageBox.Show("No workers are available to do the job '" + workerBeeJob.Text + "'", "The queen bee says...");
+            else
+                MessageBox.Show("The job '" + workerBeeJob.Text + "' will be done in " + shifts.Value + " shifts", "The queen says...");
+        }
+
+        private void nextShift_Click(object sender, EventArgs e)
+        {
+            report.Text = queen.WorkTheNextShift();
         }
     }
 }
