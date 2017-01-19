e95e0110d2427d1405d7e3a58e09f8a7e265870c
TeddyBearic <agraham41325@gmail.com>
2017-01-18 16:09:46 -0800

    Beehive and PartyManager2.0 added to this bitch

---
+++ PartyApplication/PartyApplication/DinnerParty.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyApplication
+{
+    class DinnerParty : Party
+    {
+        public bool HealthyOptions { get; set; }
+
+        public DinnerParty(int numberOfPeople, bool fancyDecorations, bool healthyOptions)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            HealthyOptions = healthyOptions;
+        }
+
+        private decimal CalculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+
+            if (FancyDecorations)
+                costOfDecorations = (NumberOfPeople * 15) + 50;
+            else
+                costOfDecorations = (NumberOfPeople * 7.50M) + 30;
+
+            return costOfDecorations;
+        }
+
+        private decimal CalculateCostOfBeveragesPerPersom()
+        {
+            decimal costOfBeveragesPerPerson;
+
+            if (HealthyOptions)
+                costOfBeveragesPerPerson = 5.00M;
+            else
+                costOfBeveragesPerPerson = 20.00M;
+
+            return costOfBeveragesPerPerson;
+        }
+
+        override public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = base.Cost;
+                totalCost += ((CalculateCostOfBeveragesPerPersom() + CostOfFoodPerPerson) * NumberOfPeople);
+
+                if (HealthyOptions)
+                    totalCost *= .95M;
+
+                return totalCost;
+            }
+        }
+    }
+}
---
+++ PartyApplication/PartyApplication/Party.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyApplication
+{
+    class Party
+    {
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public const int CostOfFoodPerPerson = 25;
+
+        private decimal CalculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+
+            if (FancyDecorations)
+                costOfDecorations = (NumberOfPeople * 15.00M) + 50M;
+            else
+                costOfDecorations = (NumberOfPeople * 7.5M) + 30M;
+
+            return costOfDecorations;
+        }
+
+        virtual public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += CostOfFoodPerPerson * NumberOfPeople;
+
+                if (NumberOfPeople > 12)
+                    totalCost += 100;
+
+                return totalCost;
+            }
+        }
+    }
+}
